e9ee8b7b2c90fddab13e4415ad9ba62b23a038b1
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2022-12-08 14:40:41 +0000

    Import papaparse, path, fs. Read and parse the csv

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,33 @@
 import express from "express";
 import { json } from "body-parser";
 import cors from 'cors';
+import path from 'path';
+import fs from 'fs';
+import parse from 'papaparse';
+
+// Use path.join to create a path to the csv file
+const filePath = path.join(__dirname, './../../raw-data/2004-2012.csv');
+
+// Use fs to read the file and parse it
+fs.readFile(filePath, (err, data) => {
+  if (err) throw err;
+
+  parse.parse(data.toString(), {
+    header: true,
+    complete: (results) => {
+      const entries = results.data.filter(entry => entry.Year === '2012');
+      const countries = {};
+      entries.forEach(entry => {
+        const country = entry.Country;
+        const medal = entry.Medal;
+        if (!countries[country]) countries[country] = { Gold: 0, Silver: 0, Bronze: 0 };
+        countries[country][medal]++;
+      });
+
+      console.log(countries);
+    }
+  });
+});
 
 const createApp = () => {
   const app = express();
